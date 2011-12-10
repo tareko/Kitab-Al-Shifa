768703bc82e88ea9897efe9e349a21695268a389
Tarek Loubani <tarek@tarek.org>
2011-12-09 22:52:11 -0500

    Removed testing auth code

--- app/Model/user.php
+++ app/Model/user.php
@@ -30,37 +30,6 @@ class User extends AppModel
 			)
 		),
 	);
-	
-	
-	protected function findUser($username, $password) {
-		$userModel = $this->settings['userModel'];
-
-		list($plugin, $model) = pluginSplit($userModel);
-		$fields = $this->settings['fields'];
-
-		$user = ClassRegistry::init($userModel)->findByUsername($username);
-		$parts = explode(':', $user['User']['password']);
-		$salt = $parts[1];
-		
-		$conditions = array(
-			$model . '.' . $fields['username'] => $username,
-			$model . '.' . $fields['password'] => md5($password . $salt) . ':' . $salt,
-		);
-		if (!empty($this->settings['scope'])) {
-		$conditions = array_merge($conditions, $this->settings['scope']);
-		}
-		$result = ClassRegistry::init($userModel)->find('first', array(
-		'conditions' => $conditions,
-		'recursive' => 0
-		));
-		if (empty($result) || empty($result[$model])) {
-		return false;
-		}
-		unset($result[$model][$fields['password']]);
-		return $result[$model];
-	}
-	
-	
 }
 
 ?>
