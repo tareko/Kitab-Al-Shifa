a6b682eba0947504161a914ac4d3c795de2cdb29
tareko <tarek@tarek.org>
2013-02-09 15:04:00 +0200

    Fixes issue #86

--- app/View/Helper/DatePickerHelper.php
+++ app/View/Helper/DatePickerHelper.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /** This helper is designed to allow for the selection of a date, with output for a form.
  * It requires some dates to enable, and by default allows no dates.
  * @author Tarek
@@ -9,41 +9,41 @@ class DatePickerHelper extends AppHelper {
 
 	public $helpers = array('Html', 'Form', 'Js');
 	/**
-	 * 
+	 *
 	 * @param unknown_type $shifts
 	 * @param unknown_type $calendarId
 	 */
 	function makeDatePicker($userData, $calendarId) {
 ?>
 		<?= $this->Html->css('ui-lightness/jquery-ui'); ?>
-		
+
 		<?= $this->Html->script('jquery'); // Include jQuery library ?>
-		
+
 		<?= $this->Html->script('jquery-ui'); // Include jQuery UI library ?>
-		
+
 	 	<?= $this->Html->div('', '', array('id' => 'datepicker' . $calendarId)); ?>
 
 		<script type="text/javascript">
 		$(document).ready(function() {
-		
+
 			$.getJSON(
-				'<?= $this->Html->url(array('controller' => 'shifts', 'action' => 'listShifts.json')); ?>', 
-				{id: $('<?=$userData?>').val()}, 
+				'<?= $this->Html->url(array('controller' => 'shifts', 'action' => 'listShifts.json')); ?>',
+				{id: $('<?=$userData?>').val()},
 				function(json) {
 					shiftDays = json;
-					$("#datepicker<?=$calendarId?>").datepicker({ 
-						beforeShowDay: shiftsWorking, 
+					$("#datepicker<?=$calendarId?>").datepicker({
+						beforeShowDay: shiftsWorking,
 						dateFormat: 'yy-mm-dd',
 						onSelect: calendarSelect
 					});
 				}
 			);
 		});
-		
+
 		function shiftsWorking(date) {
 		    for (i = 0; i < shiftDays.shiftList.length; i++) {
 		      dateDB = new Date(Date.UTC.apply(Date, shiftDays.shiftList[i].Shift.date.split('-').map(function (d, i) { return parseInt(d, 10) - (i === 1 ? 1 : 0); })));
-		      dateDB.setDate (dateDB.getDate()+1);
+		      dateDB.setDate (dateDB.getDate());
 		      if (date.getYear() == dateDB.getYear()
 		          && date.getMonth() == dateDB.getMonth()
 		          && date.getDate() == dateDB.getDate()) {
@@ -53,7 +53,7 @@ class DatePickerHelper extends AppHelper {
 		  return [false, ''];
 		}
 		</script>
-		<?php 
+		<?php
 		echo $this->Js->writeBuffer(); // Write cached scripts
 	}
 }
