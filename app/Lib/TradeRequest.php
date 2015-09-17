954fb6da2de0b26d34a30aa467d9b34c786a81d3
Tarek Loubani <tarek@tarek.org>
2015-09-17 14:10:15 -0400

    Consolidate functions to send emails
    All of the email-sending functions that requested action on the parts
    of originators or recipients are now consolidated into the "send" function.

--- app/Lib/TradeRequest.php
+++ app/Lib/TradeRequest.php
@@ -1,15 +1,21 @@
 <?php
 
 class TradeRequest {
+
+
+
 	/**
 	 * Send method to send email requesting trade
-	 * 
-	 * @param string $tradesDetailId
-	 * @param array $fromUser
-	 * @param array $toUser
-	 * @param string $shift
+	 * @param array $toUser  		User who will receive the email
+	 * @param array $trade  		Array with trade information
+	 * @param array $tradesDetail  	Array with details of trade (recipient)
+	 * @param string $method  		Method of communication
+	 * @param string $template 		Template used to send email
+	 * @param string $subject 		Subject line
+	 * @return multitype:boolean string
 	 */
-	public function send($tradesDetailId, $fromUser, $toUser, $shift, $method) {
+	
+	public function send($toUser = array(), $trade = array(), $tradesDetail = array(), $method = 'email', $template = false, $subject = false) {
 		App::uses('CakeEmail', 'Network/Email');
 		App::uses('TimeHelper', 'View/Helper');
 		
@@ -20,107 +26,25 @@ class TradeRequest {
 		
 		if ($method == 'email') {
 			$email = new CakeEmail('default');
-			$email->template('tradeRequest')
+			$success = $email->template($template)
 				->emailFormat('text')
 				->to($toUser['email'])
-				->subject('[Kitab] Shift trade request by ' .$fromUser['name'])
-				->viewVars(array(
-						'fromUser' => $fromUser, 
-						'toUser' => $toUser, 
-						'shift' => $shift,
-						'tradesDetailId' => $tradesDetailId,
-						'token' => $token))
-				->send();
-		}
-		$returnArray = array(
-						'return' => true,
-						'token' => $token
-		);
-		
-		return $returnArray;
-	}
-
-	/**
-	 * Send email to originator of trade
-	 * 
-	 * @param unknown_type $trade
-	 * @param unknown_type $method
-	 */
-	public function sendOriginator($trade = array(), $method = 'email') {
-		App::uses('CakeEmail', 'Network/Email');
-		App::uses('TimeHelper', 'View/Helper');
-	
-		//Generate token
-		$token = bin2hex(openssl_random_pseudo_bytes(16));
-		
-		//Send out communication to receiving user
-	
-		if ($method == 'email') {
-			$email = new CakeEmail('default');
-			$success = $email->template('tradeRequestOriginator')
-				->emailFormat('text')
-				->to($trade['User']['email'], $trade['User']['name'])
-				->subject('[Kitab] Request to trade your shift')
+				->subject('[Kitab] '. $subject)
 				->viewVars(array(
 						'user' => $trade['User'],
 						'shift' => $trade['Shift'],
 						'trade' => $trade['Trade'],
-						'tradesDetail' => $trade['TradesDetail'],
+						'tradesDetail' => (empty($tradesDetail) ? $trade['TradesDetail'] : $tradesDetail),
 						'submittedUser' => $trade['SubmittedUser'],
 						'token' => $token))
 				->send();
 		}
-		$returnArray = array(
-				'return' => ($success ? true : false),
-				'token' => $token
-				);
 		
-		return $returnArray;
+		return array(
+						'return' => ($success ? true : false),
+						'token' => $token
+		);
 	}
-
-	/*
-	 * For cases where there is pre-agreement (confirmed == 1), a message will be sent to originator and recipient confirming shift trade
-	 */
-	public function sendOriginatorRecipientConfirmed($trade = array(), $method = 'email') {
-		App::uses('CakeEmail', 'Network/Email');
-		App::uses('TimeHelper', 'View/Helper');
-
-		if ($method == 'email') {
-		
-			//Send out communication to originating user
-			$email = new CakeEmail('default');
-			$success1 = $email->template('tradeCompleteConfirmedOriginator')
-			->emailFormat('text')
-			->to($trade['User']['email'])
-			->subject('[Kitab] [pre-Confirmed] Shift trade has been made')
-			->viewVars(array(
-					'user' => $trade['User'],
-					'shift' => $trade['Shift'],
-					'trade' => $trade['Trade'],
-					'submittedUser' => $trade['SubmittedUser'],
-					'tradesDetail' => $trade['TradesDetail']))
-					->send();
-				
-			//Send out communication to receiving user
-		
-			$success2 = $email->template('tradeCompleteConfirmedRecipient')
-			->emailFormat('text')
-			->to($trade['User']['email'])
-			->subject('[Kitab] [pre-Confirmed] Shift trade has been made')
-			->viewVars(array(
-					'user' => $trade['User'],
-					'shift' => $trade['Shift'],
-					'trade' => $trade['Trade'],
-					'submittedUser' => $trade['SubmittedUser'],
-					'tradesDetail' => $trade['TradesDetail']))
-					->send();
-
-			$success = ($success1 && $success2 ? true : false);
-
-		}
-		return $success;
-	}
-	
 	
 	/**
 	 * sendOriginatorStatusChange method
