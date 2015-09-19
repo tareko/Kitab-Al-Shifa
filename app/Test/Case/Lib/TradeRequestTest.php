3cb94973c3f2557682aa0fd8507364e6bd026f59
Tarek Loubani <tarek@tarek.org>
2015-09-19 15:47:23 -0400

    Modest advances in test suite. Still woefully incomplete

--- app/Test/Case/Lib/TradeRequestTest.php
+++ app/Test/Case/Lib/TradeRequestTest.php
@@ -2,37 +2,23 @@
 App::import('Lib', 'TradeRequest');
 
 class TradeRequestTest extends CakeTestCase {
-	
+
 	public $fixtures = array('app.shift', 'app.user', 'app.profile', 'app.usergroup', 'app.group', 'app.user_usergroup_map', 'app.shifts_type', 'app.location', 'app.calendar', 'app.trade', 'app.user_usergroup_map_jem5', 'app.usergroup_jem5', 'app.trades_detail');
-	
+
 
 /**
  * test memory usage
  *
  * @return void
  */
-	public function testSend() {
-		
-	}
 
-	public function testsendOriginator() {
-		
-		$this->_TradeRequest = new TradeRequest();
-		App::uses('CakeEmail', 'Network/Email');
-		
-		$CakeEmail = $this->getMock('CakeEmail' , array('send'));
+	public function testsend() {
 
-		$CakeEmail->expects($this->any())
+		// Mock email function
+		$this->_TradeRequest = $this->getMock('_TradeRequest', array('send'));
+		$this->_TradeRequest->expects($this->any())
 		->method('send')
-		->will($this->returnValue(false));
-
-		$email = new CakeEmail('default');
-		
-		debug($CakeEmail
-				->emailFormat('text')
-				->to('test@test.com')
-				->subject('[Kitab] Shift trade request by you')
-				->send());
+		->will($this->returnValue(array('return' => true)));
 
 		$user = array(
 				'email' => 'test@test.com',
@@ -44,31 +30,8 @@ class TradeRequestTest extends CakeTestCase {
 						'Location' => array(
 								'location' => 'test location'),
 						'times' => '18-20h'));
-				
-		$result = $this->_TradeRequest->sendOriginator(5, $user, $shift, 'email');
-		debug($result);
-		debug('Please review debug.log for contents of email');
-	}
 
-	public function testsendOriginatorRecipientConfirmed() {
-		
-		$this->_TradeRequest = new TradeRequest();
-		App::uses('CakeEmail', 'Network/Email');
-		
-		$CakeEmail = $this->getMock('CakeEmail' , array('send'));
-
-		$CakeEmail->expects($this->any())
-		->method('send')
-		->will($this->returnValue(false));
-		
-		$user = array(
-				'email' => 'test@test.com',
-				'name' => 'Test user',
-				);
-		$shift = array(
-				'date' => '2015-01-24',
-				'ShiftsType' => 1);
-		$result = $this->_TradeRequest->sendOriginatorRecipientConfirmed(5, $user, $shift, 'email');
-		debug($result);
+		$result = $this->_TradeRequest->send(5, $user, $shift, 'email');
+		$this->assertEqual($result['return'], true);
 	}
 }
