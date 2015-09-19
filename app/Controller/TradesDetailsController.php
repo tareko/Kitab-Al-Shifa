8e5fe58e0daedd45736fb506a655fd3ad146675f
Tarek Loubani <tarek@tarek.org>
2015-09-19 16:38:54 -0400

    Update flash sessions

--- app/Controller/TradesDetailsController.php
+++ app/Controller/TradesDetailsController.php
@@ -7,7 +7,7 @@ App::uses('AppController', 'Controller');
  */
 class TradesDetailsController extends AppController {
 
-	
+
 	public $helpers = array('Js', 'Cache', 'PhysicianPicker', 'DatePicker', 'Time');
 	public $components = array('RequestHandler', 'Search.Prg');
 	public $scaffold = 'admin';
@@ -18,37 +18,37 @@ class TradesDetailsController extends AppController {
 	public function accept() {
 		$return = $this->changeStatus($this->request, 2);
 		if ($return == true) {
-			return $this->Session->setFlash(__('You have successfully accepted the trade.'));
+			return $this->Flash->success(__('You have successfully accepted the trade.'));
 		}
 		else {
 			return $return;
 		}
 		$this->render();
 	}
-	 
+
 	public function reject() {
 		$return = $this->changeStatus($this->request, 3);
 		if ($return == true) {
-			return $this->Session->setFlash(__('You have rejected this trade.'));
+			return $this->Flash->alert(__('You have rejected this trade.'));
 		}
 		else {
 			return $return;
 		}
 		$this->render();
 	}
-	
-	
+
+
 	public function changeStatus($request, $status) {
 		if (!isset($this->request) || !isset($this->request->query['id']) || !isset($this->request->query['token'])) {
 			throw new NotFoundException(__('Invalid trade or trade parameters missing'));
 		}
-		
+
 		App::import('Lib', 'TradeRequest');
 		$this->_TradeRequest = new TradeRequest();
-		
+
 		$token = $request->query['token'];
 		$id = $request->query['id'];
-		
+
 		$tradesDetail = $this->TradesDetail->find('first', array(
 					'fields' => array(
 						'TradesDetail.id',
@@ -96,23 +96,23 @@ class TradesDetailsController extends AppController {
 						)
 					)
 		));
-		
+
 		if (empty($tradesDetail)) {
 			throw new NotFoundException(__('Trade not found or already acted upon'));
 		}
 
-		// Check to see if the trade has already been taken by somebody else. If so, 
+		// Check to see if the trade has already been taken by somebody else. If so,
 		// then render that page and exit.
 		if ($this->alreadyTaken($tradesDetail)) {
 			$this->Session->setFlash(__('Shift already taken!'));
 			return $this->render('alreadyTaken');
 		}
-		
+
 		if ($token == $tradesDetail['TradesDetail']['token']) {
 			$this->TradesDetail->read(null, $id);
 			$this->TradesDetail->set('status', $status);
 			if ($this->TradesDetail->save()) {
- 
+
 				$this->_TradeRequest->sendRecipientStatusChange($status, $tradesDetail);
 				CakeLog::write('TradeRequest', '[TradesDetail][id]: ' .$tradesDetail['TradesDetail']['id'] . '; Changed status to '. $status);
 				return true;
@@ -127,7 +127,9 @@ class TradesDetailsController extends AppController {
 			CakeLog::write('TradeRequest', '[TradesDetail][id]: ' .$tradesDetail['TradesDetail']['id'] . '; Wrong token');
 		}
 	}
-	
+
+	// Tests if the trade is already complete or another user has taken it
+
 	public function alreadyTaken($tradesDetail) {
 
 		if ($tradesDetail['Trade']['status'] !=  1
@@ -137,9 +139,9 @@ class TradesDetailsController extends AppController {
 		foreach ($tradesDetail['Trade']['TradesDetail'] as $detail) {
 			if ($detail['status'] == 2) {
 				return true;
-			} 
+			}
 		}
-		
+
 		return false;
 	}
 }
