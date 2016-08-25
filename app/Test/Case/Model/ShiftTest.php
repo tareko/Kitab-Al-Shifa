8da8800256fc237750414e9e90ff7af847257ada
Tarek Loubani <shopping@tarek.org>
2016-08-25 09:23:09 +0300

    Still trying to fix this test

--- app/Test/Case/Model/ShiftTest.php
+++ app/Test/Case/Model/ShiftTest.php
@@ -50,8 +50,8 @@ class ShiftTestCase extends CakeTestCase {
     	$model->expects($this->any())
         ->method('lookupUserId')
         ->will($this->returnValue(1));
-
-		$result = $this->Shift->import(APP . 'Test' . DS . 'Files' . DS . 'shift-import.csv', 1);
+    	debug(APP . 'Test' . DS . 'Files' . DS . 'shift-import.csv');
+		$result = $this->Shift->import('test.csv', 1);
 		$expected = array(
 			'user_id' => (int) 1,
 			'date' => '2011-12-3',
