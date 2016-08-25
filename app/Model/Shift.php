e730209ff5d74de296d18add599885d33a9b7c90
Tarek Loubani <shopping@tarek.org>
2016-08-25 09:51:29 +0300

    try using raw for variable

--- app/Model/Shift.php
+++ app/Model/Shift.php
@@ -281,16 +281,15 @@ class Shift extends AppModel {
 	/**
 	 * Import function to import shifts into database
 	 */
-	function import ($filename = null, $calendar = null, $discard = 3) {
+	function import ($filename = null, $calendar_raw = null, $discard = 3) {
 
 		// Get start dates for file
 		App::uses('Calendar', 'Model');
 		$this->Calendar = new Calendar();
-		if (is_null($calendar) || empty($calendar) || empty($filename) || is_null($filename)) {
+		if (is_null($calendar_raw) || !isset($calendar_raw) || empty($calendar_raw) || empty($filename) || is_null($filename)) {
 			return false;
 		}
-		echo $calendar;
-		$calendar = $this->Calendar->getStartEndDates($calendar);
+		$calendar = $this->Calendar->getStartEndDates($calendar_raw);
 		if ($calender == false || empty($calendar)) {
 			return false;
 		}
