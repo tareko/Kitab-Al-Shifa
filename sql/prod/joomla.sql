8cdce76ad03cdc34fffcddfc9ea18fc13d975823
Tarek Loubani <tarek@tarek.org>
2020-02-24 01:48:19 -0500

    Make production DB more solid and split things into where they belong

--- sql/prod/joomla.sql
+++ sql/prod/joomla.sql
@@ -15,6 +15,10 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+// Use Joomla database
+
+USE 'joomla';
+
 --
 -- Table structure for table `jem5_accessmanager_config`
 --
