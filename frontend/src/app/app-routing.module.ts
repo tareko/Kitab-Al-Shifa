0e5d9787ef8e4f525815be13bc7697787238b9e3
Tarek Loubani <shopping@tarek.org>
2020-03-03 19:29:48 +0200

    Change to proper routed pages

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ShiftListComponent } from './shifts/shift-list/shift-list.component';
+import { ShiftCreateComponent } from './shifts/shift-create/shift-create.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ShiftListComponent },
+  { path: 'create', component: ShiftCreateComponent },
+  // { path: 'edit', component: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
