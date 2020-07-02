9b42acd13531d62e18bef7302753689365032fb7
Tarek Loubani <shopping@tarek.org>
2020-07-01 20:54:43 -0400

    Create ENV file with mongo URI

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -16,7 +17,7 @@ var authRouter = require('./routes/auth');
 var app = express();
 
 // Initialize Mongoose connection
-mongoose.connect("mongodb://myapp:myapp@localhost:27017/kitab", {useNewUrlParser: true})
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true})
   .then(() => {
     console.log('Connected to database');
   })
